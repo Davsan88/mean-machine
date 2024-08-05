18b4b0cf3ecf24b46c8e4981f184a2a2a2bf6143
DavSan <david.sanchez.martinez@outlook.com>
2024-08-05 15:35:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const decimalNumbers = [2.3, 1.4, 7.5, 8.6, 9.1]; // Create an Array of Decimal 
 // Create a Function to Calculate the Mean
 function calculateMean(numbers) {
     let sum = 0;
-     for ()
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
 }
