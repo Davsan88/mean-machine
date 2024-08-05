8934bc3736e6281218f689309c31d246bdec211c
DavSan <david.sanchez.martinez@outlook.com>
2024-08-05 15:31:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const decimalNumbers = [2.3, 1.4, 7.5, 8.6, 9.1]; // Create an Array of Decimal Numbers
 
 // Create a Function to Calculate the Mean
-function meanMachine(decimalNumbers) {
-    const sum = 0 
+function calculateMean(numbers) {
+    let sum = 0;
+     for ()
 }
